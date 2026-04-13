832c5233940c33ec7eeacabd2ccfca19c3442fe8
Thomas <huytgce170732@fpt.edu.vn>
2026-04-13 15:52:56 +0700

    feat: add updateUserLocal reducer to manage user updates in Redux state

--- src/redux/api/userApi.ts
+++ src/redux/api/userApi.ts
@@ -1,6 +1,7 @@
 import type { User } from '../../types';
 import { baseApi } from './baseApi';
 import { setCredentials } from '../slides/auth/authSlide';
+import { updateUserLocal } from '../slides/user/userSlide';
 
 export const userApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -17,8 +18,10 @@ export const userApi = baseApi.injectEndpoints({
       async onQueryStarted(_, { dispatch, queryFulfilled }) {
         try {
           const { data } = await queryFulfilled;
-          // Cập nhật Redux state với user mới
+          // Cập nhật Redux authSlide
           dispatch(setCredentials({ user: data.user }));
+          // Cập nhật Redux userSlide
+          dispatch(updateUserLocal(data.user));
         } catch (error) {
           console.error('Failed to update avatar:', error);
         }
